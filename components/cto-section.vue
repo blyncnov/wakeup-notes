a7ef01075283286c4d1f271d4be2af481fca7549
blyncnov <bholuwatife00@gmail.com>
2024-12-24 17:15:46 +0100

    Notes App

--- components/cto-section.vue
+++ components/cto-section.vue
@@ -1,6 +1,41 @@
 <template>
     <div class="w-full">
         <Container>
+
+            <div
+                class="w-full grid grid-cols-1 py-2 md:grid-cols-2 rounded-xl text-secondary gap-2 md:gap-6 mb-12 md:mb-16">
+                <div class="w-full">
+                    <h3 class="text-2xl md:text-3xl !leading-[1.2] font-medium">
+                        <span class="hidden md:block"> Discover and Secure Your World of Notes. </span> Connect
+                        Memories to Places.
+                    </h3>
+                </div>
+
+
+                <div class="w-full">
+                    <p class="text-[18px] opacity-70 font-normal mb-3">
+                        With seamless geolocation integration, this app allows you to leave notes that are tied to
+                        specific locations.
+                    </p>
+
+                    <div class="w-full flex">
+                        <NuxtLink to="/notes"
+                            class="w-full md:w-auto justify-center group text-sms bg-secondary flex items-center gap-2 text-white rounded-xl px-4 py-2 border border-secondary">
+                            <span>Discover Notes</span>
+                            <span>
+                                <svg xmlns=" http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="lucide lucide-arrow-right  group-hover:translate-x-0.5 transition-transform duration-300">
+                                    <path d="M5 12h14" />
+                                    <path d="m12 5 7 7-7 7" />
+                                </svg>
+                            </span>
+                        </NuxtLink>
+                    </div>
+                </div>
+            </div>
+
             <div class="w-full relative bg-primary text-white rounded-xl overflow-hidden">
                 <div class="w-full md:px-10 px-3 md:py-5 py-10">
                     <div class="w-full">
--- components/features.vue
+++ components/features.vue
@@ -12,18 +12,6 @@ const Features = [
         id: 3,
         title: "Location-Based Reminders",
         description: "Set reminders that trigger notifications when you arrive at or leave a specific location, helping you stay on top of tasks and plans.",
-    }, {
-        id: 4,
-        title: "Public Note Discovery",
-        description: "Explore notes left by others based on proximity and connect with the community through location-based content.",
-    }, {
-        id: 6,
-        title: "Customizable Notifications",
-        description: "Receive alerts when you're near a location with saved notes or when a new note is added nearby.",
-    }, {
-        id: 7,
-        title: "Smart Filtering",
-        description: "Easily search and sort notes by proximity, date, or content type, simplifying navigation and organization.",
     },
 ]
 
@@ -40,40 +28,6 @@ export default {
 <template>
     <div class="w-full">
         <Container>
-            <div class="w-full grid grid-cols-1 md:grid-cols-2 rounded-xl text-secondary gap-2 md:gap-6 mb-12 md:mb-16">
-                <div class="w-full">
-                    <h3 class="text-2xl md:text-3xl !leading-[1.2] font-medium">
-                        <span class="hidden md:block"> Discover and Secure Your World of Notes. </span> Connect
-                        Memories to Places.
-                    </h3>
-                </div>
-
-
-                <div class="w-full">
-                    <p class="text-[18px] opacity-70 font-normal mb-3">
-                        With seamless geolocation integration, this app allows you to leave notes that are tied to
-                        specific locations.
-                    </p>
-
-                    <div class="w-full flex">
-                        <NuxtLink to="/notes"
-                            class="w-full md:w-auto justify-center group text-sms bg-secondary flex items-center gap-2 text-white rounded-xl px-4 py-2 border border-secondary">
-                            <span>Discover Notes</span>
-                            <span>
-                                <svg xmlns=" http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="lucide lucide-arrow-right  group-hover:translate-x-0.5 transition-transform duration-300">
-                                    <path d="M5 12h14" />
-                                    <path d="m12 5 7 7-7 7" />
-                                </svg>
-                            </span>
-                        </NuxtLink>
-                    </div>
-                </div>
-            </div>
-
-
             <div class="w-full bg-secondary rounded-xl text-white">
                 <div class="w-full min-w-full max-w-full">
                     <!-- Paper Cuts -->
@@ -90,7 +44,7 @@ export default {
                     <section class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:p-5 p-3 pt-0">
                         <div class="w-full h-full" v-for="feature in Features" :key="feature.id">
                             <div
-                                class="w-full h-full shadow-sm p-4 rounded-xl bg-primary border-b-4 border-r-2 hover:translate-y-[4px] hover:border-b flex flex-col gap-2 transition-all duration-300">
+                                class="w-full h-full shadow-sm p-4 rounded-xl bg-primary border-b-4 border-r-2 flex flex-col gap-2 transition-all duration-300">
                                 <h2 class="text-xl !leading-[1.3]">{{ feature.title }}</h2>
                                 <p class="text-[15px] text-white/90">
                                     {{ feature.description }}

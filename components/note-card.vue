10ac56c4ad0dbd32d80cfbcb8916deeb7f16a4f6
blyncnov <bholuwatife00@gmail.com>
2025-01-01 18:07:54 +0100

    Notes|private unlock features

--- components/note-card.vue
+++ components/note-card.vue
@@ -27,7 +27,7 @@ export default {
         </div>
 
         <div class="w-auto flex">
-            <NuxtLink :to="`/notes/${note.id}`">
+            <NuxtLink :to="`/notes/${note.id}`" v-if="note.note_visibility.toLowerCase() === 'public'">
                 <button
                     class="w-auto group bg-secondary border border-secondary flex !text-sm justify-center items-center gap-x-2 rounded-xl px-3 py-1.5 text-gray-100">
                     <span>Open Note</span>
@@ -41,6 +41,19 @@ export default {
                     </span>
                 </button>
             </NuxtLink>
+
+            <button v-else
+                class="w-auto group bg-secondary border border-secondary flex !text-sm justify-center items-center gap-x-2 rounded-xl px-3 py-1.5 text-gray-100">
+                <span>Open Note</span>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-lock">
+                        <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+                        <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+                    </svg>
+                </span>
+            </button>
         </div>
     </div>
 </template>

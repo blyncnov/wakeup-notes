9075fed4cca4e9ff393314271fb246638af98d34
blyncnov <bholuwatife00@gmail.com>
2025-01-02 10:19:33 +0100

    Notes | production API TEST

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -2,8 +2,8 @@ export default defineEventHandler((event) => {
   return {
     version: "v1.0.0",
     Name: "Spartial Notes",
-    connect: process.env.DATABASE_URL,
-    pulse: process.env.PULSE_API_KEY,
+    // connect: process.env.DATABASE_URL,
+    // pulse: process.env.PULSE_API_KEY,
     Description:
       "Leave a note at your favorite spot, and it will be waiting for you when you return!",
   };
